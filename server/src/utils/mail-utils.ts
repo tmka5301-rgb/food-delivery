13fd4835e95eca39a2044cf95df8a201259a2982
tmka5301-rgb <tmka5301@gmail.com>
2026-02-09 11:39:15 +0800

    h

--- server/src/utils/mail-utils.ts
+++ server/src/utils/mail-utils.ts
@@ -4,34 +4,52 @@ import { Resend } from "resend";
 
 configDotenv();
 
-const { AUTH_EMAIL, AUTH_PASS } = process.env;
+// const { AUTH_EMAIL, AUTH_PASS } = process.env;
 
-const transport = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: AUTH_EMAIL,
-    pass: AUTH_PASS,
-  },
-});
+// const transport = nodemailer.createTransport({
+//   service: "gmail",
+//   auth: {
+//     user: AUTH_EMAIL,
+//     pass: AUTH_PASS,
+//   },
+// });
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const verifyUserEmail = async (receiver: string, verifyLink: string) => {
   console.log(receiver, verifyLink);
-  await transport.sendMail({
-    from: "onboarding@resend.dev",
+  //   await resend.emails.send({
+  //     from: "Onboarding@resend.dev",
+  //     to: receiver,
+  //     subject: "Verify user",
+  //     html: `<div
+  //   style="
+  //     width: 300px;
+  //     height: 250px;
+  //     border-radius: 8px;
+  //     background-color: aqua;
+  //   "
+  // >
+  //  <a href="${verifyLink}" style="font-size: 18px; color: red">Verify user</a>
+  // </div>
+  // `,
+  //   });
+  const { data, error } = await resend.emails.send({
+    from: "Acme <onboarding@resend.dev>",
     to: receiver,
-    subject: "Verify user",
+    subject: "Verify User",
     html: `<div
-  style="
-    width: 300px;
-    height: 250px;
-    border-radius: 8px;
-    background-color: aqua;
-  "
->
- <a href="${verifyLink}" style="font-size: 18px; color: red">Verify user</a>
-</div>
-`,
+    style="
+      width: 300px;
+      height: 250px;
+      border-radius: 8px;
+      background-color: aqua;
+    "
+  >
+   <a href="${verifyLink}" style="font-size: 18px; color: red">Verify user</a>
+  </div>
+  `,
+    replyTo: "onboarding@resend.dev",
   });
+  console.log(data, error);
 };
